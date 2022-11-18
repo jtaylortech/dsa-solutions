96171d7d859383da30898f4395dbe52eb851527f
Jarred Taylor <jarrede20@gmail.com>
2022-11-18 13:01:04 -0500

    answering sorting questions

--- sorting/selection-sort.py
+++ sorting/selection-sort.py
@@ -1,12 +1,12 @@
 def selectionSort(array):
     # set current index to 0 because we start at the beginning of the array
-    # while loop will continue until we reach the end of the array
     # set smallest index to current index because we assume the current index is the smallest
+    currentIdx = 0
+    # while loop will continue until we reach the end of the array
     # for loop will continue until we reach the end of the array
     # if the value at the current index is greater than the value at the smallest index then we set the smallest index to the current index
     # after the for loop we swap the value at the current index with the value at the smallest index
     # increment the current index by 1 and repeat the process
-    currentIdx = 0
     while currentIdx < len(array) - 1:
         smallestIdx = currentIdx
         for idx in range(currentIdx + 1, len(array)):
