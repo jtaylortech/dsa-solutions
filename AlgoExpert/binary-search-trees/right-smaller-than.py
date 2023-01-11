b30fbe2b09dcdfa351adcd444a8354bb42c2c2f6
Jarred Taylor <jarrede20@gmail.com>
2023-01-10 19:11:00 -0600

    restructured folders

--- AlgoExpert/binary-search-trees/right-smaller-than.py
+++ AlgoExpert/binary-search-trees/right-smaller-than.py
@@ -1,4 +1,11 @@
 # O(n^2) time | O(n) space - where n is the length of the array
+
+# rightSmallerCounts stores the number of elements to the right of the current element that are smaller than the current element in an array
+# we can use a brute force approach to solve this problem
+# for loop to iterate through the array, rightSmallerCount is a variable that stores the number of elements to the right of the current element that are smaller than the current element
+# for loop to iterate through the array starting at the next element to the right of the current element and ending at the last element in the array
+# if the current element is greater than the element at the current index of the inner for loop, then increment rightSmallerCount by 1
+# append rightSmallerCount to rightSmallerCounts
 def rightSmallerThan(array):
     rightSmallerCounts = []
     for idx in range(len(array)):
